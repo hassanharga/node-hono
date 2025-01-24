26f2e909b1c873ef50cff153dd50beb853f4b3cc
HaSsan <hassanharga@gmail.com>
2025-01-24 15:55:18 +0200

    initial commit add eslint configure global path from ts config

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+const port = 3000;
+// eslint-disable-next-line no-console
+console.log(`Server is running on http://localhost:${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
