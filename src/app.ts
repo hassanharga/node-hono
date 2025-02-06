7220a74dd9906d212d2a5ca27c00b56d614910fe
HaSsan <hassanharga@gmail.com>
2025-02-06 17:33:31 +0200

    refactor app.ts to improve route handling and export AppRouterType

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
-import tasks from '@/routes/tasks';
-
 import configureOpenApi from '@/lib/configure-open-api';
 import createApp from '@/lib/create-app';
 import index from '@/routes/index.route';
+import tasks from '@/routes/tasks';
 
 const app = createApp();
 
-const routes = [index, tasks];
-
 configureOpenApi(app);
 
+const routes = [index, tasks] as const;
 routes.forEach((route) => {
   app.route('/', route);
 });
+
+const _routes = app.route('/', index).route('/tasks', tasks);
+// export type AppRouterType = typeof _routes;
+
+export type AppRouterType = typeof routes[number];
 
 export default app;
